26424851d2dac026a2bea3233d04428ec190fa8b
Roman Zenka <zenka.roman@mayo.edu>
2011-12-07 17:19:56 -0600

    Fixing raw2mgf test on linux.

--- services/raw2mgf/src/test/java/edu/mayo/mprc/raw2mgf/TestRawToMgf.java
+++ services/raw2mgf/src/test/java/edu/mayo/mprc/raw2mgf/TestRawToMgf.java
@@ -22,10 +22,23 @@ import java.io.IOException;
 public class TestRawToMgf {
 	private static final Logger LOGGER = Logger.getLogger(TestRawToMgf.class);
 	private File tempRootDir;
+	private File extractMsnFolder;
+	private File unixXvfbWrapper;
+	private File rawFolder;
 
 	@BeforeClass()
 	public void setUp() throws IOException {
 		tempRootDir = FileUtilities.createTempFolder();
+		extractMsnFolder = Installer.extractMsn(null, Installer.Action.INSTALL);
+		unixXvfbWrapper = Installer.xvfbWrapper(null, Installer.Action.INSTALL);
+		rawFolder = Installer.rawFiles(null, Installer.Action.INSTALL);
+	}
+
+	@AfterClass
+	public void tearDown() {
+		Installer.extractMsn(extractMsnFolder, Installer.Action.UNINSTALL);
+		Installer.xvfbWrapper(unixXvfbWrapper, Installer.Action.UNINSTALL);
+		Installer.rawFiles(rawFolder, Installer.Action.UNINSTALL);
 	}
 
 	@Test
@@ -40,28 +53,25 @@ public class TestRawToMgf {
 
 	private void runRawToMgf(int spectrumBatchSize) {
 		File raw2MgfTemp = null;
-		File extractMsnFolder = Installer.extractMsn(null, Installer.Action.INSTALL);
-		File unixXvfbWrapper = Installer.xvfbWrapper(null, Installer.Action.INSTALL);
-		File rawFolder = Installer.rawFiles(null, Installer.Action.INSTALL);
 
-		final File mgfFile = new File(tempRootDir, "result.mgf");
+		final File mgfFile = new File(tempRootDir, "result.mgf" );
 		try {
-			raw2MgfTemp = new File(tempRootDir, "raw2mgf");
+			raw2MgfTemp = new File(tempRootDir, "raw2mgf" );
 
 			FileUtilities.ensureFolderExists(raw2MgfTemp);
 
-			File rawFile = new File(rawFolder, "test.RAW");
+			File rawFile = new File(rawFolder, "test.RAW" );
 			FileUtilities.ensureFileExists(rawFile);
-			LOGGER.info("Raw file created. Raw file [" + rawFile.getAbsolutePath() + "]");
+			LOGGER.info("Raw file created. Raw file [" + rawFile.getAbsolutePath() + "]" );
 
 			final RawToMgfWorker.Config raw2mgfConfig = new RawToMgfWorker.Config("tempFolder", "wineconsole",
-					unixXvfbWrapper.getAbsolutePath(), new File(extractMsnFolder, "extract_msn.exe").getAbsolutePath());
+					unixXvfbWrapper.getAbsolutePath(), new File(extractMsnFolder, "extract_msn.exe" ).getAbsolutePath());
 
 			raw2mgfConfig.setTempFolder(FileUtilities.DEFAULT_TEMP_DIRECTORY.getAbsolutePath());
 			// The config comes pre-set for linux, on Windows we switch the wrappers off
 			if (FileUtilities.isWindowsPlatform()) {
 				raw2mgfConfig.setWrapperScript(null);
-				raw2mgfConfig.setXvfbWrapperScript("");
+				raw2mgfConfig.setXvfbWrapperScript("" );
 			}
 			ResourceFactory<RawToMgfWorker.Config, Worker> factory = new RawToMgfWorker.Factory();
 			RawToMgfWorker simpleDaemonWorker = (RawToMgfWorker) factory.create(raw2mgfConfig, null);
@@ -75,7 +85,7 @@ public class TestRawToMgf {
 			simpleDaemonWorker.processRequest(workPacket, new ProgressReporter() {
 
 				public void reportStart() {
-					LOGGER.info("Started processing");
+					LOGGER.info("Started processing" );
 				}
 
 				public void reportProgress(ProgressInfo progressInfo) {
@@ -83,8 +93,8 @@ public class TestRawToMgf {
 				}
 
 				public void reportSuccess() {
-					Assert.assertTrue(mgfFile.exists(), "MGF file was not created by raw to mgf converter worker.");
-					Assert.assertTrue(mgfFile.length() > 0, "MGF file was created, but it is empty.");
+					Assert.assertTrue(mgfFile.exists(), "MGF file was not created by raw to mgf converter worker." );
+					Assert.assertTrue(mgfFile.length() > 0, "MGF file was created, but it is empty." );
 				}
 
 				public void reportFailure(Throwable t) {
@@ -98,16 +108,13 @@ public class TestRawToMgf {
 			if (mgfFile.exists()) {
 				FileUtilities.cleanupTempFile(mgfFile);
 			}
-			Installer.extractMsn(extractMsnFolder, Installer.Action.UNINSTALL);
-			Installer.xvfbWrapper(unixXvfbWrapper, Installer.Action.UNINSTALL);
-			Installer.rawFiles(rawFolder, Installer.Action.UNINSTALL);
 		}
 	}
 
 	@Test
 	public void singleDtaTest() throws IOException {
-		final File mgfFile = new File(tempRootDir, "singleDta.mgf");
-		final File dtaFile = new File(tempRootDir, "test.10.20.30.dta");
+		final File mgfFile = new File(tempRootDir, "singleDta.mgf" );
+		final File dtaFile = new File(tempRootDir, "test.10.20.30.dta" );
 		Files.write("10 20", dtaFile, Charsets.US_ASCII);
 		final File[] files = new File[]{dtaFile};
 
@@ -119,16 +126,16 @@ public class TestRawToMgf {
 
 	@Test
 	public void zeroDtaTest() throws MprcException, IOException {
-		final File mgfFile = new File(tempRootDir, "singleDta.mgf");
+		final File mgfFile = new File(tempRootDir, "singleDta.mgf" );
 		try {
 			final File[] files = new File[]{};
 
 			DTAToMGFConverter.convert(mgfFile, files, false);
 		} catch (MprcException e) {
 			Assert.assertTrue(
-					e.getMessage().contains("MS2") &&
-							e.getMessage().contains("missing") &&
-							e.getMessage().contains("threshold"), "Exception message does not mention potential causes of the issue: " + e.getMessage());
+					e.getMessage().contains("MS2" ) &&
+							e.getMessage().contains("missing" ) &&
+							e.getMessage().contains("threshold" ), "Exception message does not mention potential causes of the issue: " + e.getMessage());
 		} finally {
 			final boolean mgfCreated = mgfFile.exists();
 			FileUtilities.quietDelete(mgfFile);
@@ -138,17 +145,17 @@ public class TestRawToMgf {
 
 	@Test
 	public void parameterCleanupTest() {
-		Assert.assertEquals(RawToMgfWorker.cleanupFromToParams("-E100 -T200 -Fabc -F1 -L999 -Lhello -f3 -l30"), "-E100 -T200 -Fabc -Lhello", "All -f# and -l# must be removed");
-		Assert.assertEquals(RawToMgfWorker.cleanupFromToParams(""), "", "All -f# and -l# must be removed");
-		Assert.assertEquals(RawToMgfWorker.cleanupFromToParams("  -L10  -F20"), "", "All -f# and -l# must be removed");
+		Assert.assertEquals(RawToMgfWorker.cleanupFromToParams("-E100 -T200 -Fabc -F1 -L999 -Lhello -f3 -l30" ), "-E100 -T200 -Fabc -Lhello", "All -f# and -l# must be removed" );
+		Assert.assertEquals(RawToMgfWorker.cleanupFromToParams("" ), "", "All -f# and -l# must be removed" );
+		Assert.assertEquals(RawToMgfWorker.cleanupFromToParams("  -L10  -F20" ), "", "All -f# and -l# must be removed" );
 	}
 
 	@Test
 	public void getParameterTest() {
-		Assert.assertEquals(RawToMgfWorker.getParamValue("-F", "-K10 -B20 -faq33 -F -F13 -l20 -f17").longValue(), 13L, "Ignore non-numeric entries, return first instance of a parameter");
-		Assert.assertEquals(RawToMgfWorker.getParamValue("-F", "-K10 -B20 -faq33 -F -Ff13 -l20 --f17"), null, "None of the parameters are in correct format");
-		Assert.assertEquals(RawToMgfWorker.getParamValue("-F", "-K10 -B20 -faq33 -F -F1.3 -l20 -f17").longValue(), 17L, "Incorrect param, uses the value specified later");
-		Assert.assertEquals(RawToMgfWorker.getParamValue("-F", "-K10 -B20 -faq33 -F -F1.3 -l20 -f1.7"), null, "None of the parameters are in correct format");
+		Assert.assertEquals(RawToMgfWorker.getParamValue("-F", "-K10 -B20 -faq33 -F -F13 -l20 -f17" ).longValue(), 13L, "Ignore non-numeric entries, return first instance of a parameter" );
+		Assert.assertEquals(RawToMgfWorker.getParamValue("-F", "-K10 -B20 -faq33 -F -Ff13 -l20 --f17" ), null, "None of the parameters are in correct format" );
+		Assert.assertEquals(RawToMgfWorker.getParamValue("-F", "-K10 -B20 -faq33 -F -F1.3 -l20 -f17" ).longValue(), 17L, "Incorrect param, uses the value specified later" );
+		Assert.assertEquals(RawToMgfWorker.getParamValue("-F", "-K10 -B20 -faq33 -F -F1.3 -l20 -f1.7" ), null, "None of the parameters are in correct format" );
 	}
 
 	@AfterClass()
