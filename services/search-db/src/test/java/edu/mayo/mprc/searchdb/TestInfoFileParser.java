47cdf903c2cdf1d04f1fbc8fce093078dc78c300
Roman Zenka <zenka.roman@mayo.edu>
2012-03-09 13:33:23 -0600

    Fixing date assert.

--- services/search-db/src/test/java/edu/mayo/mprc/searchdb/TestInfoFileParser.java
+++ services/search-db/src/test/java/edu/mayo/mprc/searchdb/TestInfoFileParser.java
@@ -1,11 +1,11 @@
 package edu.mayo.mprc.searchdb;
 
 import edu.mayo.mprc.utilities.ResourceUtilities;
+import org.joda.time.DateTime;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.Reader;
-import java.util.GregorianCalendar;
 
 /**
  * Checks that we can parse the .RAW.info.tsv files properly.
@@ -24,7 +24,7 @@ public class TestInfoFileParser {
 		Assert.assertEquals(data.getMs3PlusSpectra(), 1);
 		Assert.assertEquals(data.getInstrumentName(), "LTQ Orbitrap");
 		Assert.assertEquals(data.getInstrumentSerialNumber(), "1063B");
-		Assert.assertEquals(data.getStartTime(), new GregorianCalendar(2010, 4 - 1, 8, 18, 17, 49).getTime());
+		Assert.assertEquals(data.getStartTime(), new DateTime(2010, 4, 8, 18, 17, 49));
 		Assert.assertEquals(data.getRunTimeInSeconds(), 4500.49);
 		Assert.assertEquals(data.getComment(), "yeast TD 60K, top5, 1ug in 100uL, inj 5uL,");
 		Assert.assertEquals(data.getSampleId(), "post Ch2&Ch1 flush; std config");
