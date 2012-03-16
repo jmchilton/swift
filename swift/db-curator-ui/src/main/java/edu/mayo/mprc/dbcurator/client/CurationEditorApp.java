c495bfd0a1142b3e16e5f48d2c9222e44fd53ade
Roman Zenka <zenka.roman@mayo.edu>
2012-03-16 18:06:54 -0500

    Disable the test code for running just DB curator.

--- swift/db-curator-ui/src/main/java/edu/mayo/mprc/dbcurator/client/CurationEditorApp.java
+++ swift/db-curator-ui/src/main/java/edu/mayo/mprc/dbcurator/client/CurationEditorApp.java
@@ -10,19 +10,23 @@ import java.util.HashMap;
  */
 public final class CurationEditorApp implements EntryPoint {
 
+	private static final boolean TESTING = false;
+
 	/**
 	 * this is called when we loadPanel the module (kicks off the application)
 	 */
 	public void onModuleLoad() {
-		final RootPanel rootPanel = RootPanel.get("db-curator");
-		HashMap<String, String> userMap = new HashMap<String, String>(3);
-		userMap.put("test@test.com", "Tester Testovich Testinov");
-		CurationEditor editor = new CurationEditor(0, "test@test.com", userMap, new EditorCloseCallback() {
-			@Override
-			public void editorClosed(Integer openCurationID) {
+		if (TESTING) {
+			final RootPanel rootPanel = RootPanel.get("db-curator");
+			HashMap<String, String> userMap = new HashMap<String, String>(3);
+			userMap.put("test@test.com", "Tester Testovich Testinov");
+			CurationEditor editor = new CurationEditor(0, "test@test.com", userMap, new EditorCloseCallback() {
+				@Override
+				public void editorClosed(Integer openCurationID) {
 
-			}
-		});
-		rootPanel.add(editor);
+				}
+			});
+			rootPanel.add(editor);
+		}
 	}
 }
