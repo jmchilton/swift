837b353f7ed750426425011e2b825bf353bd6296
Roman Zenka <zenka.roman@mayo.edu>
2012-03-26 13:02:50 -0500

    Working on #261

--- services/scaffold3/src/main/java/edu/mayo/mprc/scaffold3/Scaffold3Worker.java
+++ services/scaffold3/src/main/java/edu/mayo/mprc/scaffold3/Scaffold3Worker.java
@@ -1,5 +1,7 @@
 package edu.mayo.mprc.scaffold3;
 
+import com.jamesmurty.utils.XMLBuilder;
+import edu.mayo.mprc.MprcException;
 import edu.mayo.mprc.config.DaemonConfig;
 import edu.mayo.mprc.config.DependencyResolver;
 import edu.mayo.mprc.config.ResourceConfig;
@@ -10,14 +12,18 @@ import edu.mayo.mprc.daemon.Worker;
 import edu.mayo.mprc.daemon.WorkerFactoryBase;
 import edu.mayo.mprc.daemon.exception.DaemonException;
 import edu.mayo.mprc.scaffold.ScaffoldLogMonitor;
+import edu.mayo.mprc.scaffoldparser.spectra.ScaffoldSpectraVersion;
 import edu.mayo.mprc.utilities.FileUtilities;
 import edu.mayo.mprc.utilities.ProcessCaller;
 import edu.mayo.mprc.utilities.progress.ProgressReporter;
 import org.apache.log4j.Logger;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Properties;
 import java.util.TreeMap;
 
 public final class Scaffold3Worker implements Worker {
@@ -38,7 +44,13 @@ public final class Scaffold3Worker implements Worker {
 	public final void processRequest(WorkPacket workPacket, ProgressReporter progressReporter) {
 		try {
 			progressReporter.reportStart();
-			process(workPacket, progressReporter);
+			if (workPacket instanceof Scaffold3WorkPacket) {
+				processSearch((Scaffold3WorkPacket) workPacket, progressReporter);
+			} else if (workPacket instanceof Scaffold3SpectrumExportWorkPacket) {
+				processSpectrumExport((Scaffold3SpectrumExportWorkPacket) workPacket, progressReporter);
+			} else {
+				throw new DaemonException("Unexpected packet type " + workPacket.getClass().getName() + ", expected " + Scaffold3WorkPacket.class.getName() + " or " + Scaffold3SpectrumExportWorkPacket.class.getName());
+			}
 			workPacket.synchronizeFileTokensOnReceiver();
 			progressReporter.reportSuccess();
 		} catch (Exception t) {
@@ -46,12 +58,13 @@ public final class Scaffold3Worker implements Worker {
 		}
 	}
 
-	private void process(final WorkPacket workPacket, final ProgressReporter progressReporter) {
-		if (!(workPacket instanceof Scaffold3WorkPacket)) {
-			throw new DaemonException("Unexpected packet type " + workPacket.getClass().getName() + ", expected " + Scaffold3WorkPacket.class.getName());
-		}
-
-		Scaffold3WorkPacket scaffoldWorkPacket = (Scaffold3WorkPacket) workPacket;
+	/**
+	 * Run Scaffold search.
+	 *
+	 * @param scaffoldWorkPacket Work to do.
+	 * @param progressReporter   Where to report progress.
+	 */
+	private void processSearch(final Scaffold3WorkPacket scaffoldWorkPacket, final ProgressReporter progressReporter) {
 		LOGGER.debug("Scaffold 3 search processing request");
 
 		File outputFolder = scaffoldWorkPacket.getOutputFolder();
@@ -61,6 +74,64 @@ public final class Scaffold3Worker implements Worker {
 		File scaffoldWorkFolder = getScaffoldBatchScript().getParentFile();
 		File scafmlFile = createScafmlFile(scaffoldWorkPacket, outputFolder);
 
+		runScaffold(progressReporter, scafmlFile, scaffoldWorkFolder, outputFolder);
+	}
+
+	/**
+	 * Run Scaffold spectrum expprt.
+	 *
+	 * @param scaffoldWorkPacket Work to do.
+	 * @param progressReporter   Where to report progress.
+	 */
+	private void processSpectrumExport(final Scaffold3SpectrumExportWorkPacket scaffoldWorkPacket, final ProgressReporter progressReporter) {
+		LOGGER.debug("Scaffold 3 spectrum export request");
+
+		File result = scaffoldWorkPacket.getSpectrumExportFile();
+		if (result.exists() && result.isFile() && result.canRead()) {
+			// The file is there. Is it the correct version?
+			if (isScaffold3SpectrumExport(result)) {
+				LOGGER.info("The spectrum export has already been performed: [" + result.getAbsolutePath() + "]");
+				return;
+			} else {
+				if (!result.delete()) {
+					throw new MprcException("Could not delete old version of Scaffold spectrum report: [" + result.getAbsolutePath() + "]");
+				}
+			}
+		}
+		File outputFolder = result.getParentFile();
+		// Make sure the parent folder is there
+		FileUtilities.ensureFolderExists(outputFolder);
+
+		File scaffoldWorkFolder = getScaffoldBatchScript().getParentFile();
+		File scafmlFile = createSpectrumExportScafmlFile(scaffoldWorkPacket, outputFolder);
+
+		runScaffold(progressReporter, scafmlFile, scaffoldWorkFolder, outputFolder);
+
+		if (!isScaffold3SpectrumExport(result)) {
+			throw new MprcException("Even after rerunning Scaffold, the spectrum report is still the old version");
+		}
+	}
+
+	/**
+	 * @param export The Scaffold spectrum export to check.
+	 * @return True if this is a proper Scaffold 3 spectrum export (not an older version).
+	 */
+	private boolean isScaffold3SpectrumExport(final File export) {
+		ScaffoldSpectraVersion version = new ScaffoldSpectraVersion();
+		version.load(export, null/*Not sure which version*/, null/* No progress reporting */);
+		// Currently if the version starts with 3, it is deemed ok
+		return version.getScaffoldVersion().startsWith("3");
+	}
+
+	/**
+	 * Execute Scaffold.
+	 *
+	 * @param progressReporter   Where to report progress.
+	 * @param scafmlFile         Scafml file driving Scaffold
+	 * @param scaffoldWorkFolder Where should Scaffold run (usually the Scaffold install folder)
+	 * @param outputFolder       Where do the Scaffold outputs go.
+	 */
+	private void runScaffold(ProgressReporter progressReporter, File scafmlFile, File scaffoldWorkFolder, File outputFolder) {
 		ProcessBuilder processBuilder = new ProcessBuilder(getScaffoldBatchScript().getAbsolutePath(), scafmlFile.getAbsolutePath())
 				.directory(scaffoldWorkFolder);
 
@@ -80,13 +151,92 @@ public final class Scaffold3Worker implements Worker {
 		}
 	}
 
-	public File createScafmlFile(Scaffold3WorkPacket workPacket, File outputFolder) {
+	/**
+	 * Make a scafml file for running Scaffold search.
+	 *
+	 * @param workPacket   Description of work to do.
+	 * @param outputFolder Where should the file be created.
+	 * @return Created scafml file.
+	 */
+	private File createScafmlFile(Scaffold3WorkPacket workPacket, File outputFolder) {
 		// Create the .scafml file
 		workPacket.getScafmlFile().getExperiment().setReportDecoyHits(isReportDecoyHits());
 		String scafmlDocument = workPacket.getScafmlFile().getDocument();
 		File scafmlFile = new File(outputFolder, workPacket.getExperimentName() + ".scafml");
 		FileUtilities.writeStringToFile(scafmlFile, scafmlDocument, true);
 		return scafmlFile;
+	}
+
+	/**
+	 * Create a .scafml file instructing Scaffold to export spectra.
+	 *
+	 * @param work         Work to do.
+	 * @param outputFolder Where to put the {@code .scafml} file.
+	 * @return The created .scafml file
+	 */
+	private File createSpectrumExportScafmlFile(Scaffold3SpectrumExportWorkPacket work, File outputFolder) {
+		final String experimentName = FileUtilities.stripGzippedExtension(work.getScaffoldFile().getName());
+		final File scafmlFile = new File(outputFolder,
+				experimentName + "_spectrum_export.scafml");
+
+		final String contents;
+		try {
+			contents = getScafmlSpectrumExport(work);
+		} catch (Exception e) {
+			throw new MprcException("Could not export " + scafmlFile.getAbsolutePath(), e);
+		}
+
+		FileUtilities.writeStringToFile(scafmlFile, contents, true);
+		return scafmlFile;
+	}
+
+	/**
+	 * @param work Spectrume export to do.
+	 * @return String to put into .scafml file that will produce the export.
+	 * @throws ParserConfigurationException If xml creation failed
+	 */
+	static String getScafmlSpectrumExport(Scaffold3SpectrumExportWorkPacket work) {
+		try {
+			final String experimentName = FileUtilities.stripGzippedExtension(work.getScaffoldFile().getName());
+			XMLBuilder builder = XMLBuilder.create("Scaffold");
+			builder.a("version", "1.5")
+					.e("Experiment")
+					.a("name", experimentName)
+					.a("load", work.getScaffoldFile().getAbsolutePath());
+
+			{
+				builder
+						.e("DisplayThresholds")
+						.a("name", "Some Thresholds")
+						.a("id", "thresh")
+						.a("proteinProbability", "0.8")
+						.a("minimumPeptideCount", "1")
+						.a("peptideProbability", "0.8")
+						.a("minimumNTT", "1")
+						.a("useCharge", "true,true,true")
+						.a("useMergedPeptideProbability", "true")
+						.up();
+
+				builder
+						.e("Export")
+						.a("type", "spectrum")
+						.a("thresholds", "thresh")
+						.a("path", work.getSpectrumExportFile().getAbsolutePath())
+						.up();
+			}
+
+			final Properties outputProperties = new Properties();
+			// Explicitly identify the output as an XML document
+			outputProperties.setProperty(OutputKeys.METHOD, "xml");
+			// Pretty-print the XML output (doesn't work in all cases)
+			outputProperties.setProperty(OutputKeys.INDENT, "yes");
+			outputProperties.setProperty("{http://xml.apache.org/xalan}indent-amount", "2");
+			outputProperties.setProperty(OutputKeys.STANDALONE, "yes");
+
+			return builder.asString(outputProperties);
+		} catch (Exception e) {
+			throw new MprcException("Could not create .scafml for spectrum export", e);
+		}
 	}
 
 
---
+++ services/scaffold3/src/test/java/edu/mayo/mprc/scaffold3/TestScaffold3Worker.java
@@ -0,0 +1,28 @@
+package edu.mayo.mprc.scaffold3;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+/**
+ * @author Roman Zenka
+ */
+public final class TestScaffold3Worker {
+	/**
+	 * The .scafml for spectrum export should be properly produced.
+	 */
+	@Test
+	public void testSpectrumExport() {
+		final File input = new File("/scaffold.sfd");
+		final File output = new File("/spectrum.spectra.txt");
+		final Scaffold3SpectrumExportWorkPacket work = new Scaffold3SpectrumExportWorkPacket("task1", false, input, output);
+		final String result = Scaffold3Worker.getScafmlSpectrumExport(work);
+		Assert.assertEquals(result, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+				"<Scaffold version=\"1.5\">\n" +
+				"  <Experiment load=\"/scaffold.sfd\" name=\"scaffold\"/>\n" +
+				"  <DisplayThresholds id=\"thresh\" minimumNTT=\"1\" minimumPeptideCount=\"1\" name=\"Some Thresholds\" peptideProbability=\"0.8\" proteinProbability=\"0.8\" useCharge=\"true,true,true\" useMergedPeptideProbability=\"true\"/>\n" +
+				"  <Export path=\"/spectrum.spectra.txt\" thresholds=\"thresh\" type=\"spectrum\"/>\n" +
+				"</Scaffold>\n");
+	}
+}
