7adcf4a0ec502ec29792ee1e0732e51465753fe6
Roman Zenka <zenka.roman@mayo.edu>
2012-04-02 12:40:57 -0500

    Switching to synchronous queue.

--- lib/daemon/src/main/java/edu/mayo/mprc/daemon/SimpleThreadPoolExecutor.java
+++ lib/daemon/src/main/java/edu/mayo/mprc/daemon/SimpleThreadPoolExecutor.java
@@ -2,8 +2,8 @@ package edu.mayo.mprc.daemon;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 public final class SimpleThreadPoolExecutor extends ThreadPoolExecutor {
 
 	public SimpleThreadPoolExecutor(final int numThreads, final String threadName, boolean blockIfFull) {
-		super(numThreads, numThreads, 1, TimeUnit.SECONDS, blockIfFull ? new ArrayBlockingQueue<Runnable>(numThreads) : new LinkedBlockingQueue<Runnable>(numThreads));
+		super(numThreads, numThreads, 1, TimeUnit.SECONDS, blockIfFull ? new SynchronousQueue<Runnable> () : new LinkedBlockingQueue<Runnable>(numThreads));
 
 		final String name = threadName == null ? "worker" : threadName;
 
