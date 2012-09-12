0b4026e68977e14af38302bebe431a3c701438da
Roman Zenka <zenka.roman@mayo.edu>
2012-09-12 09:07:47 -0500

    Xvfb is shared, keeps running.

--- swift/scripts/src/main/resources/bin/util/unixXvfbWrapper.sh
+++ swift/scripts/src/main/resources/bin/util/unixXvfbWrapper.sh
@@ -7,32 +7,37 @@ umask 002
 
 create_xvfb () {
 	USERNAME=`whoami`
-	DISPLAYNO=1
-	while [ -z $xvfb_success ]
-		do
-		echo Xvfb :${DISPLAYNO} -screen 0 1024x1024x8
-		Xvfb :${DISPLAYNO} -screen 0 1024x1024x8 >& /dev/null &
-		XVFB_PID=$!
-		echo $XVFB_PID
-		sleep 1
-		if ps --pid $XVFB_PID
-               		then
-                	echo "Started XVFB on display $DISPLAYNO process $XVFB_PID"
-                	xvfb_success=1
-                else
-		        echo "Failed to use display "${DISPLAYNO}
-                        DISPLAYNO=$(($DISPLAYNO + 1))
-                        XVFB_PID=""
-		fi
- 		done
-	export XVFB_PID
-	export DISPLAY=:${DISPLAYNO}
-	}
 
-kill_xvfb () {
-	kill $XVFB_PID
-	}
+    # Get latest running Xvfb
+    XVFB_PID=`pgrep -n -u $USERNAME ^Xvfb\$`
+    if [ ! -z $XVFB_PID ] ; then
+        echo Found running Xvfb pid $XVFB_PID
+        XVFB_DISPLAY=`ps -p $XVFB_PID -o args= | awk '{ print $2 }'`
+        echo Connecting to display $XVFB_DISPLAY
+        export DISPLAY=$XVFB_DISPLAY
+        xvfb_success=1
+    else
+        DISPLAYNO=1
+        while [ -z $xvfb_success ]; do
+            echo Xvfb :${DISPLAYNO} -screen 0 1024x1024x8
 
+            Xvfb :${DISPLAYNO} -screen 0 1024x1024x8 >& /dev/null &
+            XVFB_PID=$!
+            echo $XVFB_PID
+            sleep 1
+            if ps --pid $XVFB_PID; then
+                echo "Started XVFB on display $DISPLAYNO process $XVFB_PID"
+                xvfb_success=1
+            else
+                echo "Failed to use display "${DISPLAYNO}
+                DISPLAYNO=$(($DISPLAYNO + 1))
+                XVFB_PID=""
+            fi
+            done
+        export DISPLAY=:${DISPLAYNO}
+    fi
+    export XVFB_PID
+}
 
 if [ $# -lt 1 ]
 then
@@ -47,12 +52,10 @@ else
         exit 1
     fi
 
-       create_xvfb
+    create_xvfb
 
-       echo "Executing command: $@"
-       "$@"
+    echo "Executing command: $@"
+    "$@"
 
-       echo Killing Xvfb at $XVFB_PID
-       kill_xvfb
-       echo Done
+    echo Done. Xvfb is left running at $XVFB_PID
 fi
