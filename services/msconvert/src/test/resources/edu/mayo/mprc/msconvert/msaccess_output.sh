6ff5a7638e80db13c956ceb4d5b2adf70470a92a
Roman Zenka <zenka.roman@mayo.edu>
2012-11-01 16:05:29 -0500

    msaccess checks one more file (Orbi with MS3 spectra enabled).

--- services/msconvert/src/test/resources/edu/mayo/mprc/msconvert/msaccess_output.sh
+++ services/msconvert/src/test/resources/edu/mayo/mprc/msconvert/msaccess_output.sh
@@ -9,8 +9,9 @@ QE1=${DIR}/test/qe1_2012oct10_10_postk4_blank2.raw
 QE2=${DIR}/test/kj_qe2_2012sep26_12c_phe_inf_us5.RAW
 ELITE=${DIR}/test/Lieske_CW_090712_3.raw
 ORBI=${DIR}/test/o63_12aug03_05_uc_blank4.RAW
+ORBIMS3=${DIR}/test/testLarge1.RAW
 
-for I in $QE1 $QE2 $ELITE $ORBI; do
+for I in $QE1 $QE2 $ELITE $ORBI $ORBIMS3; do
         echo Extracting metadata for: $I
         echo --------------------------------------
         $MSACCESS -x metadata $I -o $PROJECT_DIR
