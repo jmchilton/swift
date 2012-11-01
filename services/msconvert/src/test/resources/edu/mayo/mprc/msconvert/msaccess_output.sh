beaba07b5dbf1a72f2d848c7e1942f625836b248
Roman Zenka <zenka.roman@mayo.edu>
2012-11-01 13:46:44 -0500

    Minor improvements of the msaccess test output gatherer script.

--- services/msconvert/src/test/resources/edu/mayo/mprc/msconvert/msaccess_output.sh
+++ services/msconvert/src/test/resources/edu/mayo/mprc/msconvert/msaccess_output.sh
@@ -6,13 +6,13 @@ DIR=/mnt/mprc/software/public/bumbershoot/pwiz-wine-3_0_4019/
 MSACCESS=${DIR}/run_msaccess.sh
 
 QE1=${DIR}/test/qe1_2012oct10_10_postk4_blank2.raw
-QE2=${DIR}}/test/kj_qe2_2012sep26_12c_phe_inf_us5.RAW
+QE2=${DIR}/test/kj_qe2_2012sep26_12c_phe_inf_us5.RAW
 ELITE=${DIR}/test/Lieske_CW_090712_3.raw
 ORBI=${DIR}/test/o63_12aug03_05_uc_blank4.RAW
 
-$MSACCESS -x metadata $QE1 -o $PROJECT_DIR
-$MSACCESS -x metadata $QE2 -o $PROJECT_DIR
-$MSACCESS -x metadata $ELITE -o $PROJECT_DIR
-$MSACCESS -x metadata $ORBI -o $PROJECT_DIR
-
-
+for I in $QE1 $QE2 $ELITE $ORBI; do
+        echo Extracting metadata for: $I
+        echo --------------------------------------
+        $MSACCESS -x metadata $I -o $PROJECT_DIR
+        echo Error code: $?
+done
